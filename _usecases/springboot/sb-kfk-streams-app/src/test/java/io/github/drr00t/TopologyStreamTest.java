ea9126de8460b6487df2fd03876fc330536164ae
drroot <adribeiro@gmai.com>
2025-06-23 20:18:40 -0300

    fix test

--- _usecases/springboot/sb-kfk-streams-app/src/test/java/io/github/drr00t/TopologyStreamTest.java
+++ _usecases/springboot/sb-kfk-streams-app/src/test/java/io/github/drr00t/TopologyStreamTest.java
@@ -1,19 +1,18 @@
 package io.github.drr00t;
 
-import kafka.server.CreateTopicsRequestWithPolicyTest;
-import org.apache.kafka.common.serialization.*;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.query.Position;
-import org.apache.kafka.streams.query.PositionBound;
-import org.apache.kafka.streams.query.Query;
-import org.apache.kafka.streams.state.*;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.streams.state.VersionedBytesStoreSupplier;
+import org.apache.kafka.streams.state.VersionedKeyValueStore;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.internal.Bytes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
 import java.time.Duration;
@@ -37,15 +36,15 @@ public class TopologyStreamTest {
 
 
         //source stream
-        KStream<String,String> wordCountSource = streamsBuilder
-                .stream("input-topic", Consumed.with(Serdes.String(),Serdes.String()));
+        KStream<String, String> wordCountSource = streamsBuilder
+                .stream("input-topic", Consumed.with(Serdes.String(), Serdes.String()));
 
         //processor stream
-        KStream<String,String> wordCountProcessor = wordCountSource
-                .mapValues( value -> value.toUpperCase());
+        KStream<String, String> wordCountProcessor = wordCountSource
+                .mapValues(value -> value.toUpperCase());
 
         //sink processor
-        wordCountSource.to("output-topic", Produced.with(Serdes.String(),Serdes.String()));
+        wordCountProcessor.to("output-topic", Produced.with(Serdes.String(), Serdes.String()));
 
         Topology topology = streamsBuilder.build();
 
@@ -68,14 +67,14 @@ public class TopologyStreamTest {
 
             Assertions.assertThat(outputTopic.readKeyValuesToList())
                     .containsExactly(
-                            KeyValue.pair("key", "hello world"),
-                            KeyValue.pair("key2", "hello"));
+                            KeyValue.pair("key", "hello world".toUpperCase()),
+                            KeyValue.pair("key2", "hello".toUpperCase()));
         }
     }
 
     @Test
     @DisplayName("Persist Key value store message into local store")
-    void givenInputmessages_whenMessagemBeenProcessed_thenPersistIntoLocalStore(){
+    void givenInputmessages_whenMessagemBeenProcessed_thenPersistIntoLocalStore() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         // Create a state store
         streamsBuilder.table("input-topic",
@@ -97,9 +96,9 @@ public class TopologyStreamTest {
             inputTopic.pipeInput("key", "persisted value 1");
 
             // Retrieve the local store and assert the latest value for the key
-            ReadOnlyKeyValueStore<String,String> store = topologyTestDriver.getKeyValueStore("local-store");
-            try(var itr = store.all()){
-                while(itr.hasNext()){
+            ReadOnlyKeyValueStore<String, String> store = topologyTestDriver.getKeyValueStore("local-store");
+            try (var itr = store.all()) {
+                while (itr.hasNext()) {
                     KeyValue<String, String> next = itr.next();
                 }
             }
@@ -110,14 +109,14 @@ public class TopologyStreamTest {
 
     @Test
     @DisplayName("Persist versioned message into local store")
-    void givenInputmessages_whenMessagemBeenProcessed_thenVersionedPersistIntoLocalStore(){
+    void givenInputmessages_whenMessagemBeenProcessed_thenVersionedPersistIntoLocalStore() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         // Create a state store
         // Using VersionedKeyValueStore to persist the latest version of the value
         VersionedBytesStoreSupplier versionedStoreSupplier = Stores
                 .persistentVersionedKeyValueStore("local-store-versioned", Duration.ofMinutes(1));
-        KTable<String,String> kWords = streamsBuilder.table("input-topic",
-                Consumed.with(Serdes.String(),Serdes.String()),
+        KTable<String, String> kWords = streamsBuilder.table("input-topic",
+                Consumed.with(Serdes.String(), Serdes.String()),
                 Materialized.<String, String>as(versionedStoreSupplier)
                         .withKeySerde(Serdes.String())
                         .withValueSerde(Serdes.String()));
@@ -138,7 +137,7 @@ public class TopologyStreamTest {
             inputTopic.pipeInput("key", "persisted value 1");
 
             // Retrieve the local store and assert the latest value for the key
-            VersionedKeyValueStore<String,String> store = topologyTestDriver.getVersionedKeyValueStore("local-store-versioned");
+            VersionedKeyValueStore<String, String> store = topologyTestDriver.getVersionedKeyValueStore("local-store-versioned");
 //            var query = new Query();
 //            var itr = store.get("key");
 //            var next = itr.value();
@@ -149,20 +148,20 @@ public class TopologyStreamTest {
 
     @Test
     @DisplayName("Query versioned message from local store")
-    void givenInputmessages_whenMessagemBeenProcessed_thenQueryVersionedPersistIntoLocalStore(){
+    void givenInputmessages_whenMessagemBeenProcessed_thenQueryVersionedPersistIntoLocalStore() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         // Create a state store
         // Using VersionedKeyValueStore to persist the latest version of the value
         VersionedBytesStoreSupplier versionedStoreSupplier = Stores
                 .persistentVersionedKeyValueStore("query-local-store-versioned", Duration.ofMinutes(1));
 
-        Map<String,String> changeLogConfigs = Map.of(
-                "retention.ms","172800000", // 2 days
-                    "retention.bytes", "10 000 000 000" // 10GB
-    );
+        Map<String, String> changeLogConfigs = Map.of(
+                "retention.ms", "172800000", // 2 days
+                "retention.bytes", "10 000 000 000" // 10GB
+        );
 
-        KTable<String,String> kWords = streamsBuilder.table("query-input-topic",
-                Consumed.with(Serdes.String(),Serdes.String()),
+        KTable<String, String> kWords = streamsBuilder.table("query-input-topic",
+                Consumed.with(Serdes.String(), Serdes.String()),
                 Materialized.<String, String>as(versionedStoreSupplier)
                         .withKeySerde(Serdes.String())
                         .withValueSerde(Serdes.String())
@@ -187,10 +186,7 @@ public class TopologyStreamTest {
             inputTopic.pipeInput("key", "persisted value 1");
 
             // Retrieve the local store and assert the latest value for the key
-            VersionedKeyValueStore<String,String> store = topologyTestDriver.getVersionedKeyValueStore("query-local-store-versioned");
-
-            var itr = store.query(, PositionBound.at( 0), PositionBound.at(2));
-            var next = itr.value();
+            VersionedKeyValueStore<String, String> store = topologyTestDriver.getVersionedKeyValueStore("query-local-store-versioned");
 
             Assertions.assertThat(store.get("key").value()).isEqualTo("persisted value 1");
         } catch (InterruptedException e) {
